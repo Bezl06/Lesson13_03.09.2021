3224aeeac75e5780dde51d233ccdc2a07cf6fcea
Parviz Karimov <bezlpk@gmail.com>
2021-09-03 16:47:08 +0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account() { name = "Hokim", login = "Hokim2000", password = "123123" };
+        }
+    }
+    class Account
+    {
+        public string name;
+        public string login;
+        public string password;
+    }
+}
